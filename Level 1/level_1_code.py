ed0e2e5e485adfa80f8d1c6e2b41151bfa6114ce
Dustyn P. <pruittdustyn06@gmail.com>
2025-08-07 16:52:57 -0400

    Update level_1_code.py

--- Level 1/level_1_code.py
+++ Level 1/level_1_code.py
@@ -8,6 +8,25 @@ LEVEL 1 CHALLENGE - HAPPY PRIDE MONTH
 import time
 from rich import print
 
+DefaultColor = "\033[0m"
+Blue = "\033[94m"
+Red = "\033[91m"
+Purple = "\033[95m"
+Green = "\033[92m"
+Yellow = "\033[93m"
+
 while True:
+    print(Blue + "Hello World")
+    time.sleep(.1)
+    print(Red + "Hello World")
+    time.sleep(.1)
+    print(Purple + "Hello World")
+    time.sleep(.1)
+    print(Green + "Hello World")
+    time.sleep(.1)
+    print(Yellow + "Hello World")
+    time.sleep(.1)
+    print(DefaultColor + "Hello World")
+    
 
     pass # TODO - Replace this with your code!
